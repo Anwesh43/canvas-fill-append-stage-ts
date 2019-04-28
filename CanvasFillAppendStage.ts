a18fd3a1f1e9d57150d00509366994b817248ce0
Anwesh43 <anweshthecool0@gmail.com>
2019-04-28 14:30:04 +0530

    adding drawFilledRect method

--- CanvasFillAppendStage.ts
+++ CanvasFillAppendStage.ts
@@ -7,3 +7,8 @@ const backGroundColor : string = "#bdbdbd"
 const updateScale : Function = (scale : number, dir : number) => {
     return scale * dir * scGap
 }
+
+const drawFilledRect : Function = (context : CanvasRenderingContext2D, sc : number) => {
+    context.fillStyle = foreColor
+    context.fillRect(0, 0, w, h * sc)
+}
