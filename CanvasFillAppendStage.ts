58294c4df34f9ec636fd5368afb55fa53d3d92a5
Anwesh43 <anweshthecool0@gmail.com>
2019-04-28 14:55:53 +0530

    adding FillAppendCanvasNode class

--- CanvasFillAppendStage.ts
+++ CanvasFillAppendStage.ts
@@ -3,6 +3,7 @@ const h : number = window.innerHeight
 const scGap : number = 0.05
 const foreColor : string = "#f44336"
 const backGroundColor : string = "#bdbdbd"
+const nodes : number = 5 
 
 const updateScale : Function = (scale : number, dir : number) => {
     return scale * dir * scGap
@@ -25,14 +26,14 @@ class State {
             this.scale = this.prevScale + this.dir
             this.dir = 0
             this.prevScale = this.scale
-            cb()
+            cb(this.prevScale)
         }
     }
 
     startUpdating(cb : Function) {
         if (this.dir == 0) {
             this.dir = 1 - 2 * this.prevScale
-            cb()
+            cb(this.dir)
         }
     }
 }
@@ -54,5 +55,70 @@ class Animator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class FillAppendCanvasNode {
+
+    state : State = new State()
+    prev : FillAppendCanvasNode
+    next : FillAppendCanvasNode
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    constructor(private i : number) {
+        this.addNeighbor()
+        this.initCanvas()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new FillAppendCanvasNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+    }
+
+    draw() {
+        this.context.fillStyle = backGroundColor
+        this.context.fillRect(0, 0, w, h)
+        this.context.fillStyle = foreColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    update(cb : Function) {
+        this.state.update((scale) => {
+            if (scale == -1) {
+                document.body.removeChild(this.canvas)
+            }
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating((dir) => {
+            if (dir == 1) {
+                document.body.appendChild(this.canvas)
+                window.scrollBy(0, h)
+            }
+            cb()
+        })
+    }
+
+    getNext(dir : number, cb : Function) : FillAppendCanvasNode {
+        var curr : FillAppendCanvasNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
     }
 }
